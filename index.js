0f94aa7b7ee8962eb45f9183a0d0863b6af46072
Matthew Andrews <matt@mattandre.ws>
2015-01-14 00:16:44 +0000

    update name

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function HttpError(msg) {
+function SubclassedError(msg) {
     this.message = msg;
-    this.name = 'HttpError';
+    this.name = 'SubclassedError';
     const err = Error(msg); // http://es5.github.io/#x15.11.1
     this.stack = err.stack;
 }
 
-HttpError.prototype = Object.create(Error.prototype);
-HttpError.prototype.constructor = HttpError;
+SubclassedError.prototype = Object.create(Error.prototype);
+SubclassedError.prototype.constructor = SubclassedError;
 
 var assert = require('chai').assert;
 var util = require('util');
 
 try {
-	const err = new HttpError("Test");
+	const err = new SubclassedError("Test");
 
 	assert(err instanceof Error);
-	assert(err instanceof HttpError);
-	assert.equal(err.name, 'HttpError');
+	assert(err instanceof SubclassedError);
+	assert.equal(err.name, 'SubclassedError');
 	assert(util.isError(err));
 	// These would fail if you relied on Error.call(this, msg)
 	assert.include(err.stack, __filename);
